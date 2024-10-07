9227ddb8d01604211937c48992729a489fef4ea3
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-10-06 22:19:59 -0500

    refactor (init.sh): heredocs, functions, error handling
    - switch to bash
    - heredoc for internal description
    - fix tabs
    - move majority of global conditionals/vars to functions (tbf, still global, just abstracted)

--- init.sh
+++ init.sh
@@ -1,44 +1,57 @@
-#!/bin/sh
-#
-# Install script for instantbox
-# Home Page: https://github.com/instantbox/instantbox
-#
-# Usage:
-#  mkdir instantbox && cd $_
-#  bash <(curl -sSL https://raw.githubusercontent.com/instantbox/instantbox/master/init.sh)"
-#  docker-compose up -d
-#
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2162
+
+cat << 'DESCRIPTION' >/dev/null
+Install script for instantbox
+Home Page: https://github.com/pythoninthegrass/instantbox
+
+Usage:
+	mkdir -p instantbox && cd $_
+	bash <(curl -sSL https://raw.githubusercontent.com/pythoninthegrass/instantbox/master/init.sh)"
+	docker compose up -d
+DESCRIPTION
 
 check_cmd() {
-    command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
-echo "Welcome to instantbox, please wait..."
-echo ""
+dependencies=(curl docker docker-compose)
 
-if check_cmd docker; then
-    echo "docker is installed"
-else
-    echo "docker is not installed, please try again after it's installed"
-    exit 1
-fi
+check_dependencies() {
+	for dependency in "${dependencies[@]}"; do
+		if ! check_cmd "$dependency" && [ "$dependency" != "docker-compose" ]; then
+			echo "$dependency is not installed, please try again after it's installed"
+			exit 1
+		elif [ "$dependency" == "docker-compose" ]; then
+			if ! check_cmd "$dependency"; then
+				if ! check_cmd "docker" || ! docker compose version >/dev/null 2>&1; then
+					echo "docker-compose is not installed, please try again after it's installed"
+					exit 1
+				fi
+			fi
+		fi
+	done
+}
 
-if check_cmd docker-compose; then
-    echo "docker-compose is installed"
-else
-    curl -sSL https://raw.githubusercontent.com/docker/compose/master/script/run/run.sh > /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose || exit 1
-fi
+setup_compose_file() {
+	curl -sSLO https://raw.githubusercontent.com/pythoninthegrass/instantbox/master/docker-compose.yml
+	echo "Enter your IP (optional): "
+	read IP
+	echo "Choose a port (default: 8888): "
+	read PORT
+	[[ -z "$IP" ]] || sed -i -e "s/SERVERURL=$/SERVERURL=$IP/" docker-compose.yml
+	[[ -z "$PORT" ]] || sed -i -e "s/8888:80/$PORT:80/" docker-compose.yml
+}
 
-curl -sSLO https://raw.githubusercontent.com/instantbox/instantbox/master/docker-compose.yml
+main() {
+	echo -e "Welcome to instantbox, please wait...\n"
 
-echo "Enter your IP (optional): "
-read IP
-echo "Choose a port (default: 8888): "
-read PORT
+	check_dependencies
+	setup_compose_file
 
-[  -z "$IP" ] || sed -i -e "s/SERVERURL=$/SERVERURL=$IP/" docker-compose.yml
-[  -z "$PORT" ] || sed -i -e "s/8888:80/$PORT:80/" docker-compose.yml
+	echo "You're all set! "
+	echo "Run 'docker compose up -d' then go to http://${IP:-localhost}:${PORT:-8888} on your browser."
+}
 
-echo "You're all set! "
-echo "Run 'docker-compose up -d' then go to http://${IP:-localhost}:${PORT:-8888} on your browser."
+main
